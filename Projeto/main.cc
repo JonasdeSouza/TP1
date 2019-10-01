e40099d10d9053e72bcc702ac9dc99d9cc5a2098
Jonas <Souza>
2019-09-30 21:30:17 -0300

    Projeto finalizado, falta documentação

--- Projeto/main.cc
+++ Projeto/main.cc
@@ -6,24 +6,6 @@
 using namespace std;
 
 int main(){
-    //User usuarion;
-    //Booking reservara;
-    //Ride caronana;
-    //Account contaa;
-
-    //usuarion.CadastrarUsuario("Jonas de Souza", "55-61-998137441", "jonasdesouza@gmail.com", "salve", "048.236.901-92");
-    //reservara.ReservarCarona(1234, 2, 'D');
-    //caronana.CadastrarCarona(2, 21, 8223, "Brasilia", "DF", "Sao Paulo", "SP", "22/02/2019", "5.000,00" );
-    //contaa.CadastrarInfoBancarias(170, "1234-4", "123456-6");
-
-    //usuarion.Imprime();
-    //cout << "\n" << "---------" << "\n";
-    //reservara.Imprime();
-    //cout << "\n" << "---------" << "\n";
-    //caronana.Imprime();
-    //cout << "\n" << "---------" << "\n";
-    //contaa.Imprime();
-    //cout << "\n" << "---------" << "\n";
 
     Run run;
     return 0;
